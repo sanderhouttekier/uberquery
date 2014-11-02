6895cc0bb7a26ce3fc8283e140e174d6844f23ac
Sander Houttekier <sander.houttekier@gmail.com>
2014-11-02 06:13:36 +0100

    First stub unittest time for some testin!

---
+++ test/query.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var should = require('should');
+
+describe('Query', function() {
+    before(function(done) {
+        done();
+    });
+    describe('handlers', function() {
+        it('should just work...', function (done) {
+            done();
+        });
+    });
+});
